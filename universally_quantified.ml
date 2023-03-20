b4181760e4e8d104a7e35d8ae650a34e8060f1fb
oleg <forown@yandex.ru>
2023-03-20 13:16:41 +0300

    universally quantified types

---
+++ universally_quantified.ml
@@ -0,0 +1,46 @@
+
+
+
+
+type 'a t = 
+  | List of 'a list
+  | Nested of 'a list t 
+
+module Problems = struct 
+
+  (* High order polymorphic functions  *)
+
+
+  let len f x y = f x + f y
+
+  let len: ('a -> int) -> 'a -> 'b =
+    fun f x y -> f x + f y
+  (* no 'b: the type is: ('a -> int) -> 'a -> 'a -> int *)
+
+  let x = len List.length [1] ['c']
+
+  (* recursive polymorphic functions *)
+
+  let rec depth = function
+    | List _ -> 1
+    | Nested x -> 1 + depth x
+
+end
+
+module Solution = struct
+
+  type f = { f: 'a. 'a -> int }
+
+  let len x = x.f [1;2;3] + x.f ['a'; 'b'; 'c']
+
+  let rec depth: 'a. 'a t -> int = function
+    | List _ -> 1
+    | Nested x -> 1 + depth x
+
+end
+
+let sum: 'a -> 'b -> 'c = fun x y -> x + y
+
+
+let sum: 'a 'b 'c. 'a -> 'b -> 'c =
+  fun x y -> x + y
